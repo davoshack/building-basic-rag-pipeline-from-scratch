7a1274c261444e8bc9c8d3dac7a25355c128c532
Juan D Hernandez G <juandavos941@gmail.com>
2024-12-31 12:01:01 -0500

    Add requirements and custom utils

---
+++ layers/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="basic_rag_utils",
+    version="0.0.1",
+    description="Basic RAG Utils",
+    url="",
+    author="DavosCode",
+    author_email="jdhernandez@azzertai.com",
+    license="MIT",
+    packages=["basic_rag_utils",],
+    zip_safe=False,
+)
